865a07483946b659da5d268a74cad052824e264f
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-03 15:46:36 +0100

    Se completa el ejercicio

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const prompt = require("prompt");
 const { suma, resta, multiplicación, division } = require("./calculator");
 
-const [numeroA, numeroB] = [...process.argv.slice(2)];
+prompt.start();
 
-console.log(+numeroA);
-
-console.log(+numeroB);
-
-if (!Number.isNaN(+numeroA) && !Number.isNaN(+numeroB)) {
-  console.log(suma(numeroA, numeroB));
-
-  console.log(resta(numeroA, numeroB));
-
-  console.log(multiplicación(numeroA, numeroB));
-
-  console.log(division(numeroA, numeroB));
-} else {
-  console.log("Error!");
-  prompt.start();
-  prompt.get(["numeroA", "numeroB"], (err, result) => {
-    if (err) {
-      return console.log("Error!");
-    }
+prompt.start();
+prompt.get(["numeroA", "numeroB"], (err, result) => {
+  if (err) {
+    return console.log("Error!");
+  }
+  if (!Number.isNaN(+result.numeroA) && !Number.isNaN(+result.numeroB)) {
     console.log(suma(result.numeroA, result.numeroB));
 
     console.log(resta(result.numeroA, result.numeroB));
@@ -29,5 +16,8 @@ if (!Number.isNaN(+numeroA) && !Number.isNaN(+numeroB)) {
     console.log(multiplicación(result.numeroA, result.numeroB));
 
     console.log(division(result.numeroA, result.numeroB));
-  });
-}
+  } else {
+    console.log("Error!");
+    process.exit(9);
+  }
+});
